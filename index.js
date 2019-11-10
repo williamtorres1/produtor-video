bf0afc93924d3da1bed8fea61bf2efd87b6a54da
William Torres <williamtorres074@gmail.com>
2019-11-09 22:12:39 -0300

    Adicionados o termo de pesquisa e o prefixo

---
+++ index.js
@@ -0,0 +1,29 @@
+//biblioteca readline-sync importada no comando abaixo
+const readLine = require('readline-sync');
+
+
+function start()
+{
+    const content = {};
+    content.searchTerm = askAndReturnSearchTerm();
+
+    content.prefix = askAndReturnPrefix();
+
+    function askAndReturnSearchTerm()
+    {
+        return readLine.question('Type a Wikipedia search term: ');
+    }
+
+    function askAndReturnPrefix()
+    {
+        const prefixes = ['Who is', 'What is', 'The history of'];
+        const selectedPrefixIndex = readLine.keyInSelect(prefixes, 'Choose one option: ');
+        const selectedPrefixText = prefixes[selectedPrefixIndex];
+        
+        return selectedPrefixText;
+    }
+
+    console.log(content);
+}
+
+start();
