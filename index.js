6a9c34412b05bd4289d42c6b04ee1b00ced7906e
William Torres <williamtorres074@gmail.com>
2019-11-12 15:55:04 -0300

    update text.js

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ async function start()
     };
     robots.userInput(content);
     await robots.text(content);
-    //await robots.pdfCreator(content);
-    //console.log(JSON.stringify(null, 4));
+    await robots.pdfCreator(content);
     console.log(content);
 }
 
