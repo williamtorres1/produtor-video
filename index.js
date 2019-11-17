3a4335b7475cd255c8e7d768dc99e0e844e2a641
William Torres <williamtorres074@gmail.com>
2019-11-16 21:04:28 -0300

    Iniciado robô de imagens

--- index.js
+++ index.js
@@ -10,13 +10,12 @@ const robots =
 
 async function start()
 {
-    robots.userInput();
+    //robots.userInput();
     //await robots.text();
     //await robots.pdfCreator();
-    //await robots.image();
+    await robots.image();
     const content = robots.state.load();
-    console.dir(content, {depth:null });//mantem toda a profunidade do content, basicamente um stringify
-    
+    //console.dir(content, {depth:null });//mantem toda a profunidade do content, basicamente um stringify
 }
 
 start();
--- robots/image.js
+++ robots/image.js
@@ -1,6 +1,54 @@
+const state = require('./state.js');
+
+const readLine = require('readline-sync');
+
 async function robot()
 {
+    const content = state.load();
+    const imageRobotMessage = imageMessage(content);
     
+    const options = ['Use default images [NOT RECOMMENDED]', `I will choose my own images`];
+    const selectedOption = readLine.keyInSelect(options, `Choose one option to the program continue:`);
+    switch(selectedOption)
+    {
+        case 0:
+            {
+                console.log("You choosed: 'default images'.");
+                break;
+            }
+        case 1:
+            {
+                console.log(`You choosed: 'I will choose my own images'.`);
+                //const selectedImages = await selectImages(content.sentences);
+                break;
+            }
+        case -1:
+            {
+                console.log(`You choosed [cancel], ending program...`);
+                process.exit(0);
+                console.log("testando se o process.exit(0) encerra o programa mesmo");
+                break;
+            }
+    }
+
+    function imageMessage(content)
+    {
+        initialMessage = `Your video will contain ${content.maximumSentences} sentences.\n\n`;
+        mediumMessage = `We'll request you follow the next steps...`;        
+        return initialMessage.concat(mediumMessage);
+    }
+
+    async function selectImages(sentence)
+    {
+        for(const currentSentence of sentence.text)
+        {
+            console.log(`Go to the current folder and select a image for EACH sentence`);
+            console.log(`Your current sentence is:\n${currentSentence}:`);         
+            readLine.question("When you choose a image to sentence, type anything");
+            
+        }
+        
+    }
 }
 
 module.exports = robot;
