91ab3ee2bb18b2a43b025ab587068064e09189a7
William Torres <williamtorres074@gmail.com>
2019-11-17 12:04:59 -0300

    feat: image robot finalized

--- robots/image.js
+++ robots/image.js
@@ -9,6 +9,8 @@ async function robot()
     
     const options = ['Use default images [NOT RECOMMENDED]', `I will choose my own images`];
     const selectedOption = readLine.keyInSelect(options, `Choose one option to the program continue:`);
+
+    state.save(content);
     switch(selectedOption)
     {
         case 0:
@@ -19,15 +21,13 @@ async function robot()
         case 1:
             {
                 console.log(`You choosed: 'I will choose my own images'.`);
-                //const selectedImages = await selectImages(content.sentences);
+                const selectedImages = await selectImages(content.sentences);
                 break;
             }
         case -1:
             {
                 console.log(`You choosed [cancel], ending program...`);
                 process.exit(0);
-                console.log("testando se o process.exit(0) encerra o programa mesmo");
-                break;
             }
     }
 
@@ -40,14 +40,16 @@ async function robot()
 
     async function selectImages(sentence)
     {
-        for(const currentSentence of sentence.text)
-        {
-            console.log(`Go to the current folder and select a image for EACH sentence`);
-            console.log(`Your current sentence is:\n${currentSentence}:`);         
-            readLine.question("When you choose a image to sentence, type anything");
-            
-        }
-        
+        //console.log(sentence);
+
+        sentence.forEach(element => {
+            console.log(`Go to the current folder and select a image for EACH sentence.Your current sentence is:`);
+            console.dir(`${element.text}`,{depth:null})
+
+
+            console.log(`\nObs: You can search for images on your search engine favorite. We suggest you search for something like ${content.searchTerm} + ${element.keywords}`);
+            readLine.question("When you choose a image to sentence, type ENTER");
+        });
     }
 }
 
