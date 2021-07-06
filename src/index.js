3ffad1515e5fabf60b19d4afd2fa0f03f46e5564
William Torres <contatowilliamtorres@gmail.com>
2021-07-06 14:19:44 -0300

    feat(user-input): ask and return search term

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ function start() {
   const content = {}
 
   content.searchTerm = askAndReturnSearchTerm()
-
+  content.prefix = askAndReturnPrefix()
   function askAndReturnSearchTerm() {
     return readline.question('Type a Wikipedia search term: ')
+  }
+  function askAndReturnPrefix() {
+    const prefixes = ['Who is', 'What is', 'The history of']
+    const selectPrefixIndex = readline.keyInSelect(prefixes)
+    console.log(selectPrefixIndex)
+    return readline.keyInSelect('')
   }
   console.log(content)
 }
